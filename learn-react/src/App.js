b0443e10b6117effadb9825d5617d9712aff04fa
brandonPlaza <plazab@sheridancollege.ca>
2021-12-23 21:13:04 -0500

    Added tailwind to frontend

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -21,8 +21,31 @@ class App extends React.Component{
 
 render() {
     return (
-      <div className="App">
-        <p>{this.state.apiResponse}</p>
+      <div>
+        <div className="bg-slate-500 shadow-md">
+          <div className="max-w-6xl mx-auto px-4">
+              <div className="flex justify-between">
+                <div className="flex space-x-7">
+                    <a href="#" className="flex items-center py-4 px-2">
+                      <span className="font-bold font-mono text-blue-200 text-xl">Shop App</span>
+                    </a>
+                </div>
+
+                <div className="hidden md:flex items-center space-x-1">
+                    <a href="#" className="py-4 px-2 text-blue-400 border-t-4 border-blue-200 font-medium">
+                        Browse
+                    </a>
+                    <a href="#" className="py-4 px-2 text-blue-400 border-t-4 border-blue-200 font-medium">
+                        Login
+                    </a>
+                    <a href="#" className="py-4 px-2 text-blue-400 border-t-4 border-blue-200 font-medium">
+                        Signup
+                    </a>
+                </div>
+              </div>
+          </div>
+        </div>
+        
       </div>
     );
   }
