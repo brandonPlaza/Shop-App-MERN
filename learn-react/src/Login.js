c534fdcb7a689673ba0d0b073d8da983defcd580
brandonPlaza <plazab@sheridancollege.ca>
2021-12-24 02:44:37 -0500

    Implemented navigation

---
+++ learn-react/src/Login.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// class Login extends React.Component{
+
+//   constructor(props){
+//     super(props);
+//     this.state = {apiResponse:""};
+//   }
+
+//   render(){
+//       return(
+//           <div>
+//             Spooky View
+//           </div>
+//       );
+//   }
+// }
+
+export default function Login(){
+  return(
+      <div>
+        Spooky View
+      </div>
+  );
+}
--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './Login';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="login" element={<Login />}/>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
