e95d43275d3363541a239a0e42a9873ca5fad2e4
brandonPlaza <plazab@sheridancollege.ca>
2021-12-23 23:29:14 -0500

    Designed home page

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from './logo.svg';
+import background from './images/frontPageImage.jpeg'
 import './App.css';
 
 class App extends React.Component{
@@ -22,30 +23,70 @@ class App extends React.Component{
 render() {
     return (
       <div>
-        <div className="bg-slate-500 shadow-md">
-          <div className="max-w-6xl mx-auto px-4">
+        <div className="bg-highlight shadow-md">
+          <div className="max-w-6xl mx-auto">
               <div className="flex justify-between">
-                <div className="flex space-x-7">
-                    <a href="#" className="flex items-center py-4 px-2">
-                      <span className="font-bold font-mono text-blue-200 text-xl">Shop App</span>
+                <div className="flex space-x-5">
+                    <a href="#" className="flex items-center py-4">
+                      <span className="font-bold font-mono text-primary text-3xl">Shoplux</span>
                     </a>
                 </div>
 
                 <div className="hidden md:flex items-center space-x-1">
-                    <a href="#" className="py-4 px-2 text-blue-400 border-t-4 border-blue-200 font-medium">
+                    <a href="#" className="py-4 px-2 text-tertiary border-b-4 border-primary font-medium">
                         Browse
                     </a>
-                    <a href="#" className="py-4 px-2 text-blue-400 border-t-4 border-blue-200 font-medium">
+                    <a href="#" className="py-4 px-2 text-tertiary border-b-4 border-primary font-medium">
                         Login
                     </a>
-                    <a href="#" className="py-4 px-2 text-blue-400 border-t-4 border-blue-200 font-medium">
+                    <a href="#" className="py-4 px-2 text-tertiary border-b-4 border-primary font-medium">
                         Signup
                     </a>
                 </div>
               </div>
           </div>
         </div>
-        
+
+        <div className="w-full relative center">
+          <div className="w-full h-full absolute top-0 left-0 grid grid-cols-7">
+            <div className="col-span-4"></div>
+            <div className="col-span-3 bg-white bg-opacity-40 py-1 px-3">
+              <h2 className="text-4xl pb-4 text-secondary">
+                Welcome to <span className="font-bold">Shoplux!</span>
+              </h2>
+              <div className="text-highlight text-xl">
+                Shoplux is your one stop shop in your shopping endevours. Not only do we have reputable vendours around the world
+                creating product listings, you can also buy from other user listings!
+              </div>
+
+              <div className="text-highlight text-xl pt-4">
+                This makes Shoplux both a market for consumers and a network for entrepreneurs around the world to publish their products.
+                Create an account today to start shopping and selling!
+              </div>
+            </div>
+          </div>
+          <img src={background} alt="Background" className="object-cover w-full"/>
+        </div>
+
+        <div className="flex bg-highlight h-full">
+            <div className="p-3">
+              <span className="text-primary">Connect with us:</span>
+                <ul>
+                  <li>
+                    <a href="#" className="font-mono font-semibold text-tertiary">Twitter</a>
+                  </li>
+                  <li>
+                    <a href="#" className="font-mono font-semibold text-tertiary">Facebook</a>
+                  </li>
+                  <li>
+                    <a href="#" className="font-mono font-semibold text-tertiary">Instagram</a>
+                  </li>
+                  <li>
+                    <a href="#" className="font-mono font-semibold text-tertiary">Reddit</a>
+                  </li>
+                </ul>
+            </div>
+        </div>
       </div>
     );
   }
--- learn-react/tailwind.config.js
+++ learn-react/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':'#7395AE',
+        'secondary':'#557A95',
+        'tertiary':'#B1A296',
+        'highlight':'#5D5C61',
+      },
+    },
   },
   plugins: [],
 }
