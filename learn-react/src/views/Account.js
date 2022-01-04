b5689beec0f8909dab096072e1fa86c192513980
brandonPlaza <plazab@sheridancollege.ca>
2022-01-04 10:47:52 -0500

    Changed route of logo link

--- learn-react/src/views/Account.js
+++ learn-react/src/views/Account.js
@@ -15,7 +15,7 @@ class Account extends React.Component{
                     <div className="max-w-6xl mx-auto">
                         <div className="flex justify-between">
                             <div className="flex space-x-5">
-                                <a href="/" className="flex items-center py-4">
+                                <a href="/browse" className="flex items-center py-4">
                                     <span className="font-bold font-mono text-primary text-3xl">Shoplux</span>
                                 </a>
                             </div>
